3452e8eeec8bc5213558b2e58135001c1988e5a6
Inclooder <inclooder@gmail.com>
2016-09-06 23:02:08 +0200

    functions for direct flash access

--- src/snfd_internal.c
+++ src/snfd_internal.c
@@ -11,7 +11,7 @@ void snfd_initialize_block(SNFD * snfd, SNFD_UINT16 block_number)
     memcpy(header.magic_number, SNFD_MAGIC_NUMBER, sizeof(header.magic_number));
     header.state = SNFD_BLOCK_FREE;
 
-    snfd->config.write_func(block_number * SNFD_BLOCK_SIZE, &header, sizeof(header));
+    snfd_direct_write(snfd, block_number * SNFD_BLOCK_SIZE, &header, sizeof(header));
 }
 
 /*
@@ -32,7 +32,7 @@ void snfd_write_block_pattern(SNFD * snfd, SNFD_UINT16 block_number, const void 
             snfd->buffer[j] = byte;
             current_pattern = (current_pattern + 1) % pattern_size; // Set next pattern
         }
-        snfd->config.write_func(write_loc, snfd->buffer, write_size); // Write prepared buffer to the flash memory
+        snfd_direct_write(snfd, write_loc, snfd->buffer, write_size); // Write prepared buffer to the flash memory
         i += write_size; // Move forward by number of bytes writen
     }
 }
@@ -48,7 +48,7 @@ SNFD_BOOL snfd_check_block_pattern(SNFD * snfd, SNFD_UINT16 block_number, const 
     {
         SNFD_UINT32 read_size = snfd_calc_read_size(sizeof(snfd->buffer), SNFD_BLOCK_SIZE, i);
         SNFD_UINT32 read_loc = (block_number * SNFD_BLOCK_SIZE) + i;
-        snfd->config.read_func(read_loc, snfd->buffer, read_size);
+        snfd_direct_read(snfd, read_loc, snfd->buffer, read_size);
         SNFD_UINT32 j;
         for(j = 0; j < read_size; ++j)
         {
@@ -68,7 +68,7 @@ SNFD_BOOL snfd_check_block_pattern(SNFD * snfd, SNFD_UINT16 block_number, const 
  */
 SNFD_BOOL snfd_check_block(SNFD * snfd, SNFD_UINT16 block_number)
 {
-    snfd->config.block_erase_func(block_number);
+    snfd_direct_block_erase(snfd, block_number);
     const SNFD_UINT8 clean_block_pattern[1] = { 0xFF };
     SNFD_BOOL result = snfd_check_block_pattern(snfd, block_number, clean_block_pattern, sizeof(clean_block_pattern));
     if(result != SNFD_TRUE) return SNFD_FALSE;
--- src/snfd_internal.h
+++ src/snfd_internal.h
@@ -10,5 +10,9 @@ void snfd_write_block_pattern(SNFD * snfd, SNFD_UINT16 block_number, const void 
 
 SNFD_UINT32 snfd_calc_read_size(SNFD_UINT32 read_buffer_size, SNFD_UINT32 block_size, SNFD_UINT32 offset);
 
+SNFD_ERROR snfd_direct_write(SNFD * snfd, SNFD_UINT32 destination, void * source, SNFD_UINT32 size);
+SNFD_ERROR snfd_direct_read(SNFD * snfd, SNFD_UINT32 source, void * destination, SNFD_UINT32 size);
+SNFD_ERROR snfd_direct_block_erase(SNFD * snfd, SNFD_UINT16 block_number);
+
 
 #endif /* end of include guard: SNFD_INTERNAL_H */
