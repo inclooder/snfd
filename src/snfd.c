098ca577ceb574d50cbad893d3392e7a509b526b
Inclooder <inclooder@gmail.com>
2016-10-05 17:32:20 +0200

    long lines split

--- src/snfd.c
+++ src/snfd.c
@@ -41,21 +41,23 @@ void snfd_cleanup(SNFD * snfd)
  * Returns first log if order_number == 0
  * Returns 0 if log was not found.
  */
-SNFD_UINT32 snfd_find_next_log_for_file(SNFD * snfd, SNFD_FILE_NUMBER file_nr, SNFD_UINT32 order_number)
+SNFD_UINT32 snfd_find_next_log_for_file(SNFD * snfd, 
+                                        SNFD_FILE_NUMBER file_nr, 
+                                        SNFD_UINT32 order_number)
 {
     SNFD_UINT32 next_log_number = order_number + 1;
     SNFD_BLOCK_NUMBER block_number;
     SNFD_UINT32 offset;
     SNFD_LOG log;
     SNFD_BLOCK_STATE block_state;
-    for(block_number = 0; block_number < SNFD_BLOCKS_COUNT; block_number++)
+    for(block_number = 0; block_number < SNFD_BLOCKS_COUNT; ++block_number)
     {
         if(!snfd_block_has_logs(snfd, block_number)) break;
         offset = sizeof(SNFD_BLOCK_HEADER); //Skip block header
         while(offset < SNFD_BLOCK_SIZE)
         {
             snfd_direct_read(snfd, (block_number * SNFD_BLOCK_SIZE) + offset, &log, sizeof(log));
-            if(snfd_log_is_invalid(&log)) break;
+            if(snfd_log_is_invalid(&log)) break; //Break on first invalid log
             if(log.file_number == file_nr && log.order_number == next_log_number)
             {
                 return (block_number * SNFD_BLOCK_SIZE) + offset;
@@ -70,7 +72,8 @@ SNFD_UINT32 snfd_find_next_log_for_file(SNFD * snfd, SNFD_FILE_NUMBER file_nr, S
 /*
  * Returns 0 if not found.
  */
-SNFD_UINT32 snfd_find_last_order_number_for_file(SNFD * snfd, SNFD_FILE_NUMBER file_nr)
+SNFD_UINT32 snfd_find_last_order_number_for_file(SNFD * snfd, 
+                                                 SNFD_FILE_NUMBER file_nr)
 {
     SNFD_UINT32 order_number = 0;
     while(snfd_find_next_log_for_file(snfd, file_nr, order_number) != 0)
@@ -93,7 +96,6 @@ SNFD_ERROR snfd_write_file(SNFD * snfd,
     // TODO: Split into two write operations if size > SNFD_BLOCK_SIZE - sizeof(SNFD_BLOCK_HEADER)
     SNFD_UINT32 write_loc = snfd_find_space_for_new_log(snfd, size);
 
-    
     if(write_loc == 0){
         return SNFD_ERROR_NO_SPACE_LEFT;
     }
